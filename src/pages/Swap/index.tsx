77cab89ddcceec4e5c05bba348fe33f17578ac12
Thyphex <thiagosardim@gmail.com>
2021-05-21 03:45:39 +0000

    message

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,4 +1,4 @@
-import { CurrencyAmount, JSBI, Token, Trade } from '@pancakeswap-libs/sdk'
+import { CurrencyAmount, JSBI, Token, Trade } from '@overage69/pancake-sdk-v2'
 import React, { useCallback, useContext, useEffect, useMemo, useState, useRef } from 'react'
 import { ArrowDown } from 'react-feather'
 import { CardBody, ArrowDownIcon, Button, IconButton, Text, useModal, Link, Flex } from '@pancakeswap-libs/uikit'
